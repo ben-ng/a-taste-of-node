eaf343b2faa9bd9d805999a8087007caf0ca3458
Ben Ng <me@benng.me>
2014-03-08 20:41:53 -0600

    Simplify missingno solution

--- act1/solutions/missingno.js
+++ act1/solutions/missingno.js
@@ -8,16 +8,11 @@
 */
 
 function missingno(input) {
-  if(input.length <= 1)
-    return null;
-
   input.sort();
 
-  for(var i=1, ii=input.length; i<ii; ++i) {
-    if(input[i]-input[i-1] !== 1) {
+  for(var i=1; i<input.length; ++i)
+    if(input[i]-input[i-1] !== 1)
       return input[i] - 1;
-    }
-  }
 
   return null;
 }
